cf3b7e45bf56b75144620e7bee86f763dc550ba5
yourusername <your@email.com>
2022-07-30 22:18:17 +0400

    Project setup

--- src/Components/Product.tsx
+++ src/Components/Product.tsx
@@ -1,15 +1,23 @@
 import React from 'react';
 import {IProduct} from '../types'
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+
+
 
 
 interface productProps{
     product: IProduct
 }
 
-const Product = (props:productProps) => {
+const Product = ({product}:productProps) => {
     return (
         <div>
-            <h1>Hello</h1>
+            <h1>{product.price}</h1>
         </div>
     );
 };
